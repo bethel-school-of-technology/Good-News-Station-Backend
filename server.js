a36ae4265eb901bfd693b98558f3a04941c09f5a
Nelson Chaves <nelsonchavespro@gmail.com>
2020-04-25 21:16:18 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
