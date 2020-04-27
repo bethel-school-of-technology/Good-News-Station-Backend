15c04d29a8299e31ddba214c3598da6ba43968c7
Joshua Gray <gohan3302@gmail.com>
2020-04-27 13:34:10 -0500

    comments and forgot ;

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoos = require('mongoose');
 
+//This adds fields for the user resource
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -14,13 +16,13 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    // avatar: {
-    //     type: String //not sure if going to be used yet
-    // },
+    avatar: {
+        type: String //gravatar Globally recognized avatar "gravatar.com" for more info
+    },
     date: {
         type: Date,
         default: Date.now
     }
 });
 
-module.exports = User = mongoose.model('user', UserSchema)
+module.exports = User = mongoose.model('user', UserSchema);
