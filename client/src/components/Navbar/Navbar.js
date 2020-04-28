5e2ed08626d3dc39f244e509a345f0d9548aa3dc
Charles Bartels <charlieb2898@gmail.com>
2020-04-28 09:00:27 -0500

    added navbar component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const navbar = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div>HOME PAGE</div>
+            <div>
+                <ul>
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="/">NEWSFEED</a></li>
+                    <li><a href="/">MY PROFILE</a></li>
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default navbar;
