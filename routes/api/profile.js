2c898f95829818633f00e2342f637f6783a72ad4
Joshua Gray <gohan3302@gmail.com>
2020-05-03 20:32:29 -0500

    Fixed Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -56,13 +56,28 @@ router.post(
             status
         } = req.body;
 
-        const profileFields = {
-            user: req.user.id,
-            location,
-            bio,
-            status
+        // Build profile object
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+
+        try {
+            // Using upsert option (creates new doc if no match is found):
+            let profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true, upsert: true }
+            );
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
         }
-    });
+    }
+);
+
 
 
 // @route    GET api/profile
